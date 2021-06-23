b3acff9567754b200c95d0d72660c0f1d3d59dc7
Muhammad Fayaad <fayet256@gmail.com>
2021-06-23 22:40:53 +0700

    update dashboard and home

--- main/views.py
+++ main/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/home.html')
+	isLogin = True if session(request, 'peran') else False
+	response = { 'isLogin': isLogin }
+	return render(request, 'main/home.html', response)
+
+def session(http_handler, key, value=None):
+    if value:
+        http_handler.session[key] = value
+        return http_handler
+    else:
+        returning = None
+        try:
+            returning = http_handler.session[key]
+        except Exception:
+            pass
+        return returning
