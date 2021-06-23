b564a05af8d757944c3927470f919801c1700cbf
fadlia68 <fadli.aulawi@ui.ac.id>
2021-06-23 17:27:58 +0700

    CRUD hotel, transaksi_makan & paket_makan

--- t5_makanan/urls.py
+++ t5_makanan/urls.py
@@ -3,10 +3,21 @@ from django.urls import path
 
 app_name = 't5_makanan'
 urlpatterns = [
-    path('tr-makan', tr_makan, name='tr_makan'),
     path('tr-makan/create', tr_makan_create, name='tr_makan_create'),
-    path('tr-makan/detail/<str:id_t>/<str:id_tm>', tr_makan_detail, name='tr_makan_detail'),
+    path('tr-makan/detail/<str:id_t>/<str:id_tm>',
+         tr_makan_detail, name='tr_makan_detail'),
     path('tr-makan/getKode/<str:id_t>', getKode, name='tr_makan_getKode'),
-    path('tr-makan/update/<str:id_t>/<str:id_tm>', tr_makan_update, name='tr_makan_update'),
-    path('tr-makan/delete/<str:id_t>/<str:id_tm>', tr_makan_delete, name='tr_makan_delete')
+    path('tr-makan/update/<str:id_t>/<str:id_tm>',
+         tr_makan_update, name='tr_makan_update'),
+    path('tr-makan/delete/<str:id_t>/<str:id_tm>',
+         tr_makan_delete, name='tr_makan_delete'),
+
+    path('paket-makan/create', pm_create, name="pm_create"),
+    path('paket-makan/update/<str:kh>/<str:kp>', pm_update, name="pm_update"),
+    path('paket-makan/delete/<str:kh>/<str:kp>', pm_delete, name="pm_delete"),
+
+    path('hotel/create', hotel_create, name="hotel_create"),
+    path('hotel/update/<str:kode>', hotel_update, name="hotel_update"),
+
+    path('', index, name="index"),
 ]
